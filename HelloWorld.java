0c2a1d92d8b04b66de67f09d80d4422201068d49
root <root>
2021-11-04 10:44:41 +0800

    删除第一个文件

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public void HelloWorld(){
-	public static void main(String[] args){
-		System.out.println("HelloWorld");
-		System.out.println("HelloWoqerfrwgwrrld");
-		System.out.println("HelloWoqerfrwgwrrld");
-		System.out.println("HelloWoqerfrwgwrrld");
-	}
-}
